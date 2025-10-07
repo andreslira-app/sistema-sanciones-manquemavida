af3b6c8758522a0a934be567fde98bff13f48794
andreslira-app <andresflirae@gmail.com>
2025-10-07 16:20:02 -0300

    Update app.py
    Signed-off-by: andreslira-app <andresflirae@gmail.com>

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 # Configuración
 st.set_page_config(page_title="Sugerencia de Sanciones - Colegio Manquemávida", page_icon="https://raw.githubusercontent.com/andreslira-app/sistema-sanciones-manquemavida/refs/heads/main/insignia.png")
 # Mostrar el logo del colegio
-st.image("https://raw.githubusercontent.com/andreslira-app/sistema-sanciones-manquemavida/refs/heads/main/insignia.png")
-st.title("⚖️ Colegio Manquemávida Sistema de Sugerencia de Sanciones")
+st.image("https://raw.githubusercontent.com/andreslira-app/sistema-sanciones-manquemavida/refs/heads/main/insignia.png") st.title("⚖️ Colegio Manquemávida Sistema de Sugerencia de Sanciones")
 st.markdown("Este sistema ayuda a identificar la gravedad de una falta cometida por un estudiante y sugiere una sanción proporcional a partir de IA basada en el **Reglamento Interno del Colegio Manquemávida**.")
 
 # Reglamento: lista de (palabra clave, falta, sanción, nivel)
